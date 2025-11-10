e00270ed4abaee1e12593333db0d57ac845bdc97
nicogite <ncls.legendre@mail.com>
2025-11-10 12:37:15 +0100

    Update api url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+  console.log('VITE_API_PROXY_TARGET:', env.VITE_API_PROXY_TARGET)
+  console.log('mode', mode);
   const proxyTarget = env.VITE_API_PROXY_TARGET || 'http://localhost:4000'
 
   return {
